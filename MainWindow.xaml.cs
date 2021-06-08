0e567142f6a92f1e3f91b70d6d6a91759aa6ae4f
RareDevil <k_gliese@hotmail.com>
2021-06-08 05:39:27 +0200

    Added the how to use text to the TextBlock

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,14 @@ namespace TemplateGenerator
         public MainWindow()
         {
             InitializeComponent();
-            // Testing
+            StringBuilder howToInfo = new();
+            howToInfo.AppendLine("How to use:");
+            howToInfo.AppendLine("- Fill in the template field with the text you want to have a replaced something in with the different strings.");
+            howToInfo.AppendLine("Use §§§ the places where you want the strings to be placed, you can have as many as you need.");
+            howToInfo.AppendLine();
+            howToInfo.AppendLine("- Fill in the items list with the strings you want to have replaced in the template.");
+            howToInfo.Append("Use , to seperate the string you want to use.");
+            HowToUseInfo.Text = howToInfo.ToString();
         }
 
         private void GenerateButton_OnClick(object sender, RoutedEventArgs e)
